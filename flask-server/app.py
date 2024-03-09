e1c71bdeaa066737eb31e0b3b5417b42ce811602
aleXyaO7 <theundefeatedalexyao@gmail.com>
2024-03-09 16:40:31 -0500

    a

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 import requests
 
 url = {
+    'address_to_coordinates': "https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi",
+    'coordinates_to_airport': "https://aviation-reference-data.p.rapidapi.com/airports/search",
     'one_way': "https://priceline-com-provider.p.rapidapi.com/v2/flight/departures",
     'round_trip':"https://priceline-com-provider.p.rapidapi.com/v2/flight/roundTrip",
     'region' : "https://hotels-com-provider.p.rapidapi.com/v2/regions",
@@ -10,6 +13,14 @@ url = {
 }
 
 headers = {
+    'coordinates':{
+	    "X-RapidAPI-Key": "8e140a626fmsh377461c7298588ap138272jsnf7aef24a2a62",
+	    "X-RapidAPI-Host": "address-from-to-latitude-longitude.p.rapidapi.com"
+    },
+    'airports':{
+	    "X-RapidAPI-Key": "8e140a626fmsh377461c7298588ap138272jsnf7aef24a2a62",
+	    "X-RapidAPI-Host": "aviation-reference-data.p.rapidapi.com"
+    },
     'flights':{
 	    "X-RapidAPI-Key": "8e140a626fmsh377461c7298588ap138272jsnf7aef24a2a62",
 	    "X-RapidAPI-Host": "priceline-com-provider.p.rapidapi.com"
@@ -19,6 +30,26 @@ headers = {
 	    "X-RapidAPI-Host": "hotels-com-provider.p.rapidapi.com"
     }
 }
+
+def query_coordinates(address):
+    query_string = {"address":address}
+    response = requests.get(url['address_to_coordinates'], headers=headers['coordinates'], params=query_string)
+    print(response.json())
+    return {'longitude':response.json()['Results'][0]['longitude'],
+            'latitude':response.json()['Results'][0]['latitude']}
+
+def query_airports(latitude, longitude):
+    query_string = {"lat":latitude,"lon":longitude,"radius":"100"}
+    response = requests.get(url['coordinates_to_airport'], headers=headers['airports'], params=query_string)
+    airports = []
+    for i in response.json():
+        airports.append({
+            'code':i['iataCode'],
+            'name':i['name'],
+            'latitude':i['latitude'],
+            'longitude':i['longitude']
+        })
+    return airports
 
 def query_one_way(departure_airport, destination_airport, departure_date, adults, children, cabin_class):
     querystring = {"sid":"iSiX639",
@@ -30,25 +61,26 @@ def query_one_way(departure_airport, destination_airport, departure_date, adults
                 "cabin_class":cabin_class,
                 "number_of_itineraries": 10,
                 "currency":"USD"}
-    response = requests.get(url['one_way'], headers=headers, params=querystring).json()
+    response = requests.get(url['one_way'], headers=headers['flights'], params=querystring).json()
     flights = []
-    for q in response['getAirFlightDepartures']['results']['result']['itinerary_data']:
-        f = response['getAirFlightDepartures']['results']['result']['itinerary_data'][q]
-        legs = []
-        for r in f['slice_data']['slice_0']['flight_data']:
-            i = f['slice_data']['slice_0']['flight_data'][r]
-            legs.append({'departure_airport':i['departure']['airport']['code'],
-                         'departure_date':i['departure']['datetime']['date'],
-                         'departure_time':i['departure']['datetime']['time_12h'],
-                         'arrival_airport':i['arrival']['airport']['code'],
-                         'arrival_date':i['arrival']['datetime']['date'],
-                         'arrival_time':i['arrival']['datetime']['time_12h']})
-        flights.append({
-            'airline':f['slice_data']['slice_0']['airline']['name'],
-            'logo':f['slice_data']['slice_0']['airline']['logo'],
-            'legs':legs,
-            'price':f['price_details']['display_total_fare']
-        })
+    if response['getAirFlightDepartures']:
+        for q in response['getAirFlightDepartures']['results']['result']['itinerary_data']:
+            f = response['getAirFlightDepartures']['results']['result']['itinerary_data'][q]
+            legs = []
+            for r in f['slice_data']['slice_0']['flight_data']:
+                i = f['slice_data']['slice_0']['flight_data'][r]
+                legs.append({'departure_airport':i['departure']['airport']['code'],
+                                'departure_date':i['departure']['datetime']['date'],
+                                'departure_time':i['departure']['datetime']['time_12h'],
+                                'arrival_airport':i['arrival']['airport']['code'],
+                                'arrival_date':i['arrival']['datetime']['date'],
+                                'arrival_time':i['arrival']['datetime']['time_12h']})
+            flights.append({
+                'airline':f['slice_data']['slice_0']['airline']['name'],
+                'logo':f['slice_data']['slice_0']['airline']['logo'],
+                'legs':legs,
+                'price':f['price_details']['display_total_fare']
+            })
     return flights
 
 def query_round_trip(departure_airport, destination_airport, departure_date, return_date, adults, children, cabin_class):
@@ -61,7 +93,7 @@ def query_round_trip(departure_airport, destination_airport, departure_date, ret
                 "cabin_class":cabin_class,
                 "number_of_itineraries": 10,
                 "currency":"USD"}
-    response = requests.get(url['round_trip'], headers=headers, params=querystring).json()
+    response = requests.get(url['round_trip'], headers=headers['flights'], params=querystring).json()
     flights = []
     for q in response['getAirFlightRoundTrip']['results']['result']['itinerary_data']:
         f = response['getAirFlightRoundTrip']['results']['result']['itinerary_data'][q]
@@ -95,7 +127,7 @@ def query_hotel(place, checkin_date, checkout_date, adults, children_ages, lodgi
     querystring = {"query":place,
                    "domain":"US",
                    "locale":"en_US"}
-    response = requests.get(url['region'], headers=headers, params=querystring).json()
+    response = requests.get(url['region'], headers=headers['hotels'], params=querystring).json()
 
     place_id = response['data'][0]['gaiaId']
     if children_ages:
@@ -153,9 +185,30 @@ def query_hotel(place, checkin_date, checkout_date, adults, children_ages, lodgi
 
 app = Flask(__name__)
 
-@app.route('/info')
-def info():
-    return ("hello world")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/flight', methods = ['GET'])
+def flight():
+    if request.method == 'GET':
+        start_place = request.args.get('start_place', None)
+        end_place = request.args.get('end_place', None)
+        start_date = request.args.get('start_date', None)
+        end_date = request.args.get('end_date', None)
+        adults = request.args.get('adults', None)
+        children = request.args.get('children', None)
+        cabin_class = request.args.get('cabin_class', None)
+        # start_coords = query_coordinates(start_place)
+        # start_airport = query_airports(start_coords['latitude'], start_coords['longitude'])[0]['code']
+        # end_coords = query_coordinates(end_place)
+        # end_airport = query_airports(end_coords['latitude'], end_coords['latitude'])[0]['code']
+        # q = query_one_way(start_airport, end_airport, start_date, adults, children, cabin_class)
+        q = [{'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '3:30pm', 'arrival_airport': 'IST', 'arrival_date': '2024-03-20', 'arrival_time': '9:05pm'}, {'departure_airport': 'IST', 'departure_date': '2024-03-20', 'departure_time': '11:55pm', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '1:45am'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '1:45pm', 'arrival_airport': 'IST', 'arrival_date': '2024-03-20', 'arrival_time': '7:20pm'}, {'departure_airport': 'IST', 'departure_date': '2024-03-20', 'departure_time': '11:55pm', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '1:45am'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '11:20am', 'arrival_airport': 'IST', 'arrival_date': '2024-03-20', 'arrival_time': '4:55pm'}, {'departure_airport': 'IST', 'departure_date': '2024-03-20', 'departure_time': '11:55pm', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '1:45am'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '6:50pm', 'arrival_airport': 'IST', 'arrival_date': '2024-03-21', 'arrival_time': '12:20am'}, {'departure_airport': 'IST', 'departure_date': '2024-03-21', 'departure_time': '7:35am', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '9:20am'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '5:20pm', 'arrival_airport': 'IST', 'arrival_date': '2024-03-20', 'arrival_time': '10:50pm'}, {'departure_airport': 'IST', 'departure_date': '2024-03-21', 'departure_time': '7:35am', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '9:20am'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '3:30pm', 'arrival_airport': 'IST', 'arrival_date': '2024-03-20', 'arrival_time': '9:05pm'}, {'departure_airport': 'IST', 'departure_date': '2024-03-21', 'departure_time': '7:35am', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '9:20am'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '1:45pm', 'arrival_airport': 'IST', 'arrival_date': '2024-03-20', 'arrival_time': '7:20pm'}, {'departure_airport': 'IST', 'departure_date': '2024-03-21', 'departure_time': '7:35am', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '9:20am'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '6:50pm', 'arrival_airport': 'IST', 'arrival_date': '2024-03-21', 'arrival_time': '12:20am'}, {'departure_airport': 'IST', 'departure_date': '2024-03-21', 'departure_time': '1:35pm', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '3:25pm'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '11:20am', 'arrival_airport': 'IST', 'arrival_date': '2024-03-20', 'arrival_time': '4:55pm'}, {'departure_airport': 'IST', 'departure_date': '2024-03-21', 'departure_time': '7:35am', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '9:20am'}], 'price': 625.53}, {'airline': 'Turkish Airlines', 'logo': 'https://secure.rezserver.com/public/media/img/air_logos2/TK.png', 'legs': [{'departure_airport': 'CDG', 'departure_date': '2024-03-20', 'departure_time': '5:20pm', 'arrival_airport': 'IST', 'arrival_date': '2024-03-20', 'arrival_time': '10:50pm'}, {'departure_airport': 'IST', 'departure_date': '2024-03-21', 'departure_time': '1:35pm', 'arrival_airport': 'TZX', 'arrival_date': '2024-03-21', 'arrival_time': '3:25pm'}], 'price': 625.53}]
+        print(q)
+        return q
+
+    
+
 
 
 if __name__ == '__main__':
